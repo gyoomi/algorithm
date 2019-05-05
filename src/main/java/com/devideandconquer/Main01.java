e244afb16ffc0d393415eba5805e3f75c99d5ea0
gyoomi <gyoomi0709@foxmail.com>
2019-05-05 18:06:30 +0800

    quick sort

---
+++ src/main/java/com/devideandconquer/Main01.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.devideandconquer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ *
+ * @author Leon
+ * @version 2019/5/5 15:56
+ */
+public class Main01 {
+
+    public static final int DEFAULT_PIVOT = 0;
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(0);
+        arr.add(-9);
+        arr.add(12);
+        arr.add(20);
+        arr.add(-98);
+        arr.add(76);
+        arr.add(3);
+        arr.add(88);
+        arr.add(898);
+        arr.add(-919);
+        List<Integer> rs = quickSort(arr);
+        System.out.println(rs);
+    }
+
+
+    public static List<Integer> quickSort(List<Integer> source) {
+        if (source.size() < 2) {
+            return source;
+        } else {
+            int pivot = source.get(DEFAULT_PIVOT);
+            List<Integer> less = source.stream().filter(e -> e < pivot).collect(Collectors.toList());
+            List<Integer> greater = source.stream().filter(e -> e > pivot).collect(Collectors.toList());
+            List<Integer> result = new ArrayList<>();
+            result.addAll(quickSort(less));
+            result.add(pivot);
+            result.addAll(quickSort(greater));
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
